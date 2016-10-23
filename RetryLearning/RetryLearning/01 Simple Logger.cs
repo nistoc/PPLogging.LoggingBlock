a394f507c8a0ba62e5ed562c6048c755538a8538
Nikita Temnikov <nistoc@gmail.com>
2016-10-23 17:28:33 +0300

    added simple logger

---
+++ RetryLearning/RetryLearning/01 Simple Logger.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.Practices.EnterpriseLibrary.Logging;
+using Microsoft.Practices.EnterpriseLibrary.Logging.Formatters;
+using Microsoft.Practices.EnterpriseLibrary.Logging.TraceListeners;
+
+namespace RetryLearning
+{
+    class SimpleLogger
+    {
+        /// <summary>
+        /// Настраиваем логгер
+        /// </summary>
+        public LoggingConfiguration Configure()
+        {
+            //Formatter
+            var textFormatter = new TextFormatter();
+
+            //Trace Listener - создаём и натсроиваем Файловый-Listener, который будет принимать и записывать логи 
+            // в указаннй файл, в уканном формате
+            var flatFileTraceListener = new FlatFileTraceListener(
+$@"{AppDomain.CurrentDomain.BaseDirectory}\Logs\FlatFile.log",
+@"
+^^^^^^^^^^^^^^^^^^^^^^^^",
+@"\----------------------/
+",
+textFormatter);
+
+            //Build Configuration
+            var config = new LoggingConfiguration();
+            config.AddLogSource("DiskFiles", SourceLevels.All, true)
+                .AddTraceListener(flatFileTraceListener);
+
+            return config;
+        }
+
+
+    }
+}
--- RetryLearning/RetryLearning/Program.cs
+++ RetryLearning/RetryLearning/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.Practices.EnterpriseLibrary.Logging;
 
 namespace RetryLearning
 {
@@ -10,6 +8,24 @@ namespace RetryLearning
     {
         static void Main(string[] args)
         {
+            var simpleDictData = new Dictionary<string, object>
+            {
+                {"текущая дата", DateTime.Now.ToLongDateString()},
+                {"текущее время", DateTime.Now.ToLongTimeString()}
+            };
+
+            UserDeclarativeLogger(simpleDictData);
         }
+
+        private static void UserDeclarativeLogger(Dictionary<string, object> simpleDictData)
+        {
+            var logWriterFactory = new LogWriterFactory();
+            var logWriter = logWriterFactory.Create();
+
+            logWriter.Write("пишем лог через логер-по-умолчанию", simpleDictData);
+            logWriter.Dispose();
+        }
+
+       
     }
 }
